cc1e26c2162c181cbc53831e078c6ceb95b5b1d2
QTNA <quan7colorful@outlook.com>
2024-08-11 14:00:08 +0700

    refactor: Re-decorate TaildwindCSS in 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,11 +5,11 @@ import Link from "next/link"
 
 export default function NotFound() {
     return (
-        <div className="flex h-screen flex-col justify-center items-center bg-gradient-to-bl to-blue-500 from-pink-400">
-            <div className="text-center">
-                <h1 className="text-8xl text-white">404</h1>
-                <p className="mt-2 text-white">Look like you go wrong page</p>
-                <p className="mt-1 text-white">Press the back arrow in your browser to get your previous page</p>
+        <div className="flex h-screen flex-col justify-center items-center bg-gradient-to-tr to-cyan-800 from-emerald-500">
+            <div className="bg-white text-center p-5 rounded-lg shadow-lg">
+                <h1 className="text-8xl text-black">404</h1>
+                <p className="mt-2 text-black">Look like you go wrong page</p>
+                <p className="mt-1 text-black underline">Press the back arrow in your browser to get your previous page</p>
             </div>
         </div>
     )
